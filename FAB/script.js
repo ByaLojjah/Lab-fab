c410eeef0f0f4d87a57e6fdc8a83706a02e3326a
byadiallo <zayabel99@gmail.com>
2024-05-17 16:21:01 +0000

    update

--- FAB/script.js
+++ FAB/script.js
@@ -9,7 +9,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 5000);
 }
 
 function plusSlides(n) {
