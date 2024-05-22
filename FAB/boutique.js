eb24707d4334769fbe77d2c27e815585f6f681f8
byadiallo <zayabel99@gmail.com>
2024-05-22 23:43:34 +0000

    update

--- FAB/boutique.js
+++ FAB/boutique.js
@@ -1,31 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const addToCartButtons = document.querySelectorAll('.addbutton');
 
     addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const itemId = button.getAttribute('data-id');
-            const itemName = button.getAttribute('data-name');
-            const itemDescription = button.getAttribute('data-description');
-            const itemPrice = button.getAttribute('data-price');
-
-            const item = {
-                id: itemId,
-                name: itemName,
-                description: itemDescription,
-                price: itemPrice
-            };
-
-            let cart = localStorage.getItem('cart');
-            if (cart) {
-                cart = JSON.parse(cart);
-            } else {
-                cart = [];
-            }
-
-            cart.push(item);
-            localStorage.setItem('cart', JSON.stringify(cart));
-
-            alert(`${itemName} a été ajouté au panier.`);
+        button.addEventListener('click', function () {
+            const articleId = this.getAttribute('data-id');
+            addToCart(articleId);
+            window.location.href = 'Panier.html';
         });
     });
+
+    function addToCart(articleId) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+        
+        const article = {
+            id: articleId,
+            name: document.querySelector(`button[data-id="${articleId}"]`).closest('.article').querySelector('h1').innerText,
+            description: document.querySelector(`button[data-id="${articleId}"]`).closest('.article').querySelector('.Description span').innerText,
+            price: document.querySelector(`button[data-id="${articleId}"]`).closest('.article').querySelector('.price').innerText
+        };
+
+        const articleInCart = cart.find(item => item.id === articleId);
+        if (articleInCart) {
+            articleInCart.quantity += 1;
+        } else {
+            article.quantity = 1;
+            cart.push(article);
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
 });
+
+
